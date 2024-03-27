53e2eeaa89c7441bf36c445a8b04e1d2387a7dea
Marc Beyer <marc.beyer1@gmx.de>
2024-03-27 14:17:32 +0000

    Added outline to button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,14 +3,15 @@ import { ReactNode } from "react";
 interface Props {
     children: ReactNode;
     style?: "primary" | "secondary" | "success" | "danger" | "warning" | "info" | "light" | "dark" | "link";
+    outline?: boolean;
     type?: "button" | "submit";
     className?: string;
     onClick?: () => void;
 }
 
-function Button({ children, onClick, className = "", style = "primary", type = "button" }: Props) {
+function Button({ children, onClick, className = "", style = "primary", outline = false, type = "button" }: Props) {
     return (
-        <button type={type} className={`btn btn-${style} ${className}`} onClick={onClick}>
+        <button type={type} className={`btn btn-${outline ? "outline-" : ""}${style} ${className}`} onClick={onClick}>
             {children}
         </button>
     );
