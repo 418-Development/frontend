08902a2b7d946a2a48ae640b0eb880d71e18c01e
Robert Soumagne <robert.soumagne@gmail.com>
2024-03-27 14:37:32 +0000

    small fix in imports

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import logo from '../assets/Logo418-light.png';
 import { NavigationItem } from '../enums/navigation';
