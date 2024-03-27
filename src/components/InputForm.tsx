888aa11306ab00cbd24031dfa802525de9769477
Felix <Felix.Held@edu.bib.de>
2024-03-27 17:36:02 +0000

    Fixed Length margins for Input Form

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,4 +1,5 @@
-
+import Button from "../components/Button";
+import "../index.css"
 
 function BMICalculatorForm() {
     // Placeholder function for form submission
@@ -9,10 +10,11 @@ function BMICalculatorForm() {
     //    };
 
     return (
-        <form method="post" action="/submit-bmi-data">
+        <div>
+            <form method="post" action="/submit-bmi-data">
             <div className="input-group mb-3 bg-secondary rounded">
                 <span className="input-group-text bg-dark text-white">Name</span>
-                <input type="text" name="name" className="form-control bg-dark text-white border-secondary" placeholder="Your Name" required minLength="2" maxLength="35" />
+                <input type="text" name="name" className="form-control bg-dark text-white border-secondary" placeholder="Your Name" required min="2"max="35" />
             </div>
             <div className="input-group mb-3 bg-secondary rounded">
                 <span className="input-group-text bg-dark text-white">Age</span>
@@ -27,9 +29,10 @@ function BMICalculatorForm() {
                 <input type="number" name="height" className="form-control bg-dark text-white border-secondary" placeholder="Height in centimeters" required min="0" />
             </div>
             <div className="d-flex justify-content-end">
-                <button type="submit" className="btn btn-outline-primary">Calculate BMI</button>
+                <Button type="submit">Calculate BMI</Button>
             </div>
-        </form>
+            </form>
+        </div> 
     );
 }
 
