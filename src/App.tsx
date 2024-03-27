b599680517826d33456e08679f532de6438eada8
Robert Soumagne <robert.soumagne@gmail.com>
2024-03-27 16:24:23 +0000

    Welcome screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
 import Button from "./components/Button";
 import Navigation from "./components/Navigation";
 import { NavigationItem } from "./enums/navigation";
+import BMICalculatorWelcome from "./components/BMICalculatorWelcome";
 import Users from "./routes/users";
+
+
 
 function App() {
     const navigate = useNavigate();
@@ -17,12 +20,7 @@ function App() {
                     element={
                         <>
                             <Navigation activeNavigationItem={NavigationItem.HOME} />
-                            <Button className="m-3" outline={true} style="primary" onClick={() => setCount((count) => count + 1)}>
-                                count is {count}
-                            </Button>
-                            <a href="#" onClick={() => navigate("/users")}>
-                                Users
-                            </a>
+                            <BMICalculatorWelcome />
                         </>
                     }
                 />
