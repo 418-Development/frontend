30d8d0287eaae4d686e719cd37f0c86d7361e67c
Robert Soumagne <robert.soumagne@gmail.com>
2024-03-27 14:36:14 +0000

    Implemented basic login

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     const [count, setCount] = useState(0);
 
     return (
-        <div className="container">
+        <div>
             <Routes>
                 <Route
                     path="/"
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,8 @@
-import { NavigationItem } from "../enums/navigation";
-import logo from "../assets/Logo418-light.png";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import logo from '../assets/Logo418-light.png';
+import { NavigationItem } from '../enums/navigation';
+import Button from './Button'; // Adjust the import path as necessary
 
 interface Props {
     activeNavigationItem: NavigationItem;
@@ -8,51 +10,56 @@ interface Props {
 
 function Navigation({ activeNavigationItem }: Props) {
     const navigate = useNavigate();
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+    const toggleNav = () => setIsNavCollapsed(!isNavCollapsed);
 
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <img src={logo} alt="Logo" width="30" height="24" className="d-inline-block align-text-top me-3" />
-                <a className="navbar-brand" href="#" onClick={() => navigate("/")}>
-                    418 Development
-                </a>
+            <div className="container-fluid justify-content-between">
+                <div className="d-flex align-items-center">
+                    <img src={logo} alt="Logo" width="30" height="24" className="d-inline-block align-text-top me-3" />
+                    <Button onClick={() => navigate("/")} style="link" className="navbar-brand">418 Development</Button>
+                    <Button onClick={toggleNav} className="navbar-toggler" type="button">
+                        <span className="navbar-toggler-icon"></span>
+                    </Button>
+                </div>
 
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className={`collapse navbar-collapse ${!isNavCollapsed ? 'show' : ''}`} id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <button
+                            <Button
+                                onClick={() => navigate("/")}
                                 className={`nav-link ${activeNavigationItem === NavigationItem.HOME ? "active" : ""}`}
-                                aria-current="page"
-                                onClick={() => {
-                                    navigate("/");
-                                }}
+                                style="link"
                             >
                                 Home
-                            </button>
+                            </Button>
                         </li>
                         <li className="nav-item">
-                            <button
+                            <Button
+                                onClick={() => navigate("/users")}
                                 className={`nav-link ${activeNavigationItem === NavigationItem.USERS ? "active" : ""}`}
-                                onClick={() => {
-                                    navigate("/users");
-                                }}
+                                style="link"
                             >
-                                User
-                            </button>
+                                Users
+                            </Button>
                         </li>
                     </ul>
+
+                    {!isAuthenticated ? (
+                        <div id="login-box" className="d-flex flex-column flex-lg-row align-items-center" style={{ gap: '0.5rem' }}>
+                            <input className="form-control form-control-sm mb-2 mb-lg-0 me-lg-2" type="email" id="email" autoComplete="username" placeholder="Email" aria-label="Email" style={{ height: '40px' }} />
+                            <input className="form-control form-control-sm mb-2 mb-lg-0 me-lg-2" type="password" id="password" autoComplete="current-password" placeholder="Password" aria-label="Password" style={{ height: '40px' }} />
+                            <div className="d-flex">
+                                <Button onClick={() => setIsAuthenticated(true)} style="success" outline={true} className="me-2 text-nowrap" type="submit">Login</Button>
+                                <Button onClick={() => { }} style="success" outline={true} className="text-nowrap" type="submit">Sign Up</Button>
+                            </div>
+                        </div>
+                    ) : (
+                        <Button onClick={() => setIsAuthenticated(false)} style="primary" outline={true} >Sign out</Button>
+                    )}
                 </div>
             </div>
         </nav>
