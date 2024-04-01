cdafe8140adf6190f4516fb6f8b166b2a9c8488e
Felix <Felix.Held@edu.bib.de>
2024-04-01 20:54:17 +0100

    Added some comments to the SignUpForm

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,20 +1,26 @@
 import { useRef, useState } from "react";
 import Button from "./Button";
 
+// Create an Interface Props to be able to take in username and password from the regular login page
 interface Props {
     username?: string,
     password?: string
 }
 
+// function that will validate the input of the form and send it to the backend via a post request. The Return is the dialog window that the user will interact with
 function SignUpForm({username = "", password = ""}: Props) {
+    // These constants are used with useState to remember the variables the user puts in during any reloading of the page
     const [email, setEmail] = useState<string>("");
     const [usernameInput, setUsername] = useState<string>(username);
     const [passwordInput, setPassword] = useState<string>(password);
     const [verifyPassword, setVerifyPassword] = useState<string>("");
 
+    // References to HTML elements that we want to access to inform the user of invalid password checks
     const passwordValidation = useRef<HTMLDivElement>(null)
     const passwordValidationInput = useRef<HTMLInputElement>(null)
 
+    // This const async function will check that both the password and verify password are the same. It will send the form to the backend should both be the same
+    // Should they not be the same the user will be informed of the error and the form will not send anything to the backend but wait for new user input.
     const checkPassword = async () => {
         if (passwordInput == verifyPassword) {
             signup()
@@ -25,6 +31,7 @@ function SignUpForm({username = "", password = ""}: Props) {
         }
     }
 
+    // This const async function will send the signup info to the back end via a POST request to the corresponding web address
     const signup = async () => {
         const response = await fetch("http://localhost:8080/api/auth/signup", {
             method: "POST",
@@ -45,6 +52,7 @@ function SignUpForm({username = "", password = ""}: Props) {
         console.log("json", json);
     }
 
+    // The return is the HTML structure required to display the modal dialog to the user.
     return (
         <div id="modalSignUpForm" className="modal fade">
             <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
